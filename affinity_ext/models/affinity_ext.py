501dec7dd4c53b0389c88c142418f4dafa912cbb
E Ocean <khatripawan7968@gmail.com>
2024-12-12 12:43:09 +0500

    Update affinity_ext.py

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -11,6 +11,7 @@ class ResPartnerInherited(models.Model):
     cnic_no = fields.Char("CNIC Number", required = True)
     vendor_status = fields.Selection([('active', 'Active'), ('non_active', 'Non Active')], string = "Vendor Status")
     major_client = fields.Selection([('a','A Category'), ('b', 'B Category'), ('c', 'C Category')]) 
+    code = fields.Char('Code')
 
 class ResPartnerBankInherited(models.Model):
     
