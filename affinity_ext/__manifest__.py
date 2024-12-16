705d20f4b2793f9395855c4288f4970fcfe65e66
E Ocean <73229916+BilalMemon07@users.noreply.github.com>
2024-12-16 16:26:27 +0500

    Update __manifest__.py

--- affinity_ext/__manifest__.py
+++ affinity_ext/__manifest__.py
@@ -2,7 +2,7 @@
     'name' : 'Affinity Fields Extension',
     'version' : '1.0.0',
     'summary' : 'For Customization Purpose',
-    'depends' : ['base', 'product'],
+    'depends' : ['base', 'product', 'quality_control'],
     'data' : ['views/affinity_ext.xml',],
     'installable' : True,
     'application' : True,
