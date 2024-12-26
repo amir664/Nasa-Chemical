fdd48b3704917e5c1ce78f7cb263afba30fdb16e
E Ocean <73229916+BilalMemon07@users.noreply.github.com>
2024-12-26 16:06:48 +0500

    Update controller.py

--- salesdetail_report/controllers/controller.py
+++ salesdetail_report/controllers/controller.py
@@ -43,16 +43,16 @@ class CONSReportController(http.Controller):
         if to_date != False and from_date != False:
             query += " and so.date_order between '%s' and '%s'"%(from_date, to_date)
 
-        if partner_id and partner_id!='[]':
+        if partner_id != False and partner_id!='[]':
         #    partner_id_str = partner_id.split('[')[-1].split(']')[0]
         #    partner = env['res.partner'].search([('id', '=', )])
            query += " AND rp.id = %s" % partner_id
 
            
-        if category_id and category_id!='[]':
+        if category_id != False and category_id!='[]':
         #    category_id_str = category_id.split('[')[-1].split(']')[0]
         #    raise UserError(str(category_id_str))
-           query +=  "and pt.categ_id = %s"%(category_id)
+           query +=  " and pt.categ_id = %s"%(category_id)
         
         
         if city != False:
