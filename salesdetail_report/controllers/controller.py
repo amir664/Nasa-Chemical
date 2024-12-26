853c0f8c0acc56e4e587dc177d09cdc348f45bc3
E Ocean <73229916+BilalMemon07@users.noreply.github.com>
2024-12-26 16:27:34 +0500

    Update controller.py

--- salesdetail_report/controllers/controller.py
+++ salesdetail_report/controllers/controller.py
@@ -41,9 +41,6 @@ class CONSReportController(http.Controller):
         env = http.request.env
         # partners = []
         if to_date != False and from_date != False:
-            # query += " and so.date_order between '%s' and '%s'"%(from_date, to_date)
-            from_date = datetime.strptime(from_date, '%Y,%m,%d')
-            to_date = datetime.strptime(to_date, '%Y,%m,%d') 
 
             query += " and so.date_order between '%s' and '%s'" % (from_date, to_date)
 
