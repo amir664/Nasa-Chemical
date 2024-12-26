8b0c1c45ba6bcea7866296ddd99b54a3da0e14a6
E Ocean <73229916+BilalMemon07@users.noreply.github.com>
2024-12-26 15:03:22 +0500

    Update report.py

--- salesdetail_report/report/report.py
+++ salesdetail_report/report/report.py
@@ -61,7 +61,7 @@ class CustomReport(models.AbstractModel):
                     inner join account_move am on am.invoice_origin = so.name
                     left join res_partner_res_partner_category_rel rprpc on rprpc.partner_id = rp.id
                     left join res_partner_category rpc on rpc.id = rprpc.category_id
-                    inner join product_template pt on pt.id = sol.product_template_id
+                    inner join product_template pt on pt.id = sol.product_id
                     where so.id is not null     
             """    
             )
