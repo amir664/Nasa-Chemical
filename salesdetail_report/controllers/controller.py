9fe9ed0e520380d24a92341d3176d8d4425778a8
E Ocean <73229916+BilalMemon07@users.noreply.github.com>
2024-12-26 17:32:02 +0500

    Update controller.py

--- salesdetail_report/controllers/controller.py
+++ salesdetail_report/controllers/controller.py
@@ -47,7 +47,7 @@ class CONSReportController(http.Controller):
 
             query += " and so.date_order between '%s' and '%s'" % (form_date, date_to)
 
-        raise UserError(type(partner_id) + "-- "+ str(category_id))
+        raise UserError(str(type(partner_id)) + "--"+ str(type(category_id)))
     
 
         if partner_id and partner_id != 'False':
