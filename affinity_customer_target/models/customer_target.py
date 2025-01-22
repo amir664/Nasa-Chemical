493c32fa8e8e4503cc172a4e4c196b8c71f8c760
pawankhatri123 <khatripawan7968@gmail.com>
2025-01-22 14:38:54 +0500

    ..

--- affinity_customer_target/models/customer_target.py
+++ affinity_customer_target/models/customer_target.py
@@ -5,24 +5,24 @@ class CustomerTarget(models.Model):
     _name="customer.target"
     _description="Customer Target App"
 
-    name = fields.Char(string="Name")
-    start_date = fields.Date(string="Start Date")
-    end_date = fields.Date(string="End Date")
-    line_ids = fields.One2many('customer.target.line','customer_target_id', sting="Line Ids")
-    sale_person_id = fields.Many2one('res.users',string="Sale Person ID")
-    sale_person_name = fields.Char(string="Sale Person Name")
-    region = fields.Char(string="Region")
+    name = fields.Char(string="Name", required=True)
+    start_date = fields.Date(string="Start Date", required=True)
+    end_date = fields.Date(string="End Date", required=True)
+    line_ids = fields.One2many('customer.target.line','customer_target_id', sting="Line Ids", required=True)
+    sale_person_id = fields.Many2one('res.users',string="Sale Person ID", required=True)
+    sale_person_name = fields.Char(string="Sale Person Name", required=True)
+    region = fields.Char(string="Region", required=True)
 
 class CustomerTargetLine(models.Model):
     _name="customer.target.line"
     _description = "Customer Target Line"
 
-    customer_target_id = fields.Many2one('customer.target')
-    customer = fields.Many2one('res.partner', string="Customer")
-    sales_target = fields.Float(string="Sales Target")
-    current_sales = fields.Float( string = "Current Sales")
+    customer_target_id = fields.Many2one('customer.target', required=True)
+    customer = fields.Many2one('res.partner', string="Customer", required=True)
+    sales_target = fields.Float(string="Sales Target", required=True)
+    current_sales = fields.Float( string = "Current Sales", readonly=True, required=True)
 
-    @api.onchange('customer',"customer_target_id.sale_person_id")
+    @api.depends('customer',"customer_target_id.sale_person_id")
     def get_sales_target_sum(self ):
         records = self.env['sale.order'].search([('user_id', '=', self.customer_target_id.sale_person_id.id),('partner_id', '=', self.customer.id),('state', '!=', 'cancel')])
         total_sales_target = sum(records.mapped('amount_total'))
