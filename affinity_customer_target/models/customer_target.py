ca9455bd9e0ef14070912e130d4651e1859efae3
pawankhatri123 <khatripawan7968@gmail.com>
2025-01-22 13:24:57 +0500

    ..

--- affinity_customer_target/models/customer_target.py
+++ affinity_customer_target/models/customer_target.py
@@ -9,6 +9,9 @@ class CustomerTarget(models.Model):
     start_date = fields.Date(string="Start Date")
     end_date = fields.Date(string="End Date")
     line_ids = fields.One2many('customer.target.line','customer_target_id', sting="Line Ids")
+    sale_person_id = fields.Char(string="Sale Person ID")
+    sale_person_name = fields.Char(string="Sale Person Name")
+    origin = fields.Char(string="Origin")
 
 class CustomerTargetLine(models.Model):
     _name="customer.target.line"
@@ -16,4 +19,11 @@ class CustomerTargetLine(models.Model):
 
     customer_target_id = fields.Many2one('customer.target')
     customer = fields.Many2one('res.partner', string="Customer")
-    target = fields.Float(string="Target")
+    sales_target = fields.Float(string="Sales Target")
+    current_sales = fields.Float( string = "Current Sales",  compute='_compute_current_sales', store=True)
+
+    @api.depends('sales_target')
+    def _compute_current_sales(self):
+        for record in self:
+            total = sum(self.search([]).mapped('sales_target'))
+            record.current_sales = total
