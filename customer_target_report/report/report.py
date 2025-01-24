1703f1f8f79bb9b8aa54db89ec3011b88fb4307e
pawankhatri123 <khatripawan7968@gmail.com>
2025-01-24 18:49:58 +0500

    ..

--- customer_target_report/report/report.py
+++ customer_target_report/report/report.py
@@ -7,15 +7,15 @@ class CustomReport(models.AbstractModel):
     _description = "Customer Target Report"
 
     def _get_report_values(self, docids, data=None):
-    #     customer = data['customer']
-    #     start_date = data['start_date']
-    #     end_date = data['end_date']
+        customer = data['customer']
+        start_date = data['start_date']
+        end_date = data['end_date']
 
-        # other = {
-        #     'customer': customer,
-        #     'start_date': start_date,
-        #     'end_date': end_date
-        # }
+        other = {
+            'customer': customer,
+            'start_date': start_date,
+            'end_date': end_date
+        }
 
         cr = self._cr
 
