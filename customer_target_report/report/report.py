d0dd0e7c9b730fc252af3eb72234e97fbc5780dc
pawankhatri123 <khatripawan7968@gmail.com>
2025-01-25 14:45:45 +0500

    //

--- customer_target_report/report/report.py
+++ customer_target_report/report/report.py
@@ -19,7 +19,7 @@ class CustomReport(models.AbstractModel):
             'start_date': start_date,
             'end_date': end_date
         }
-        raise UserError("Data found")
+        raise UserError(other)
 
         if customer_id != []:
             customer_id_str = ','.join(map(str,customer_id))
