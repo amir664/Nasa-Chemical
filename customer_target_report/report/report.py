b1d19a8212e5fb2f7bab49ad71db376be701a059
pawankhatri123 <khatripawan7968@gmail.com>
2025-01-25 13:08:08 +0500

    ...

--- customer_target_report/report/report.py
+++ customer_target_report/report/report.py
@@ -23,10 +23,10 @@ class CustomReport(models.AbstractModel):
         cr = self._cr
 
         query = (f"""
+                   
                     SELECT 
-                        ct.start_date, 
-                        ct.end_date,
-                        ctr.customer, 
+                        ct.start_date as start_date, 
+                        ct.end_date as end_date,
                         rp.name AS customer_name,
                         ctl.sales_target, 
                         ctl.current_sales
@@ -36,9 +36,7 @@ class CustomReport(models.AbstractModel):
                         customer_target_line AS ctl ON ct.id = ctl.customer_target_id
                     LEFT JOIN 
                         res_partner rp ON ctl.customer_target_id = rp.id
-                    LEFT JOIN
-                        customer_target_report AS ctr 
-
+                    
                  """)
         
         cr.execute(query)
