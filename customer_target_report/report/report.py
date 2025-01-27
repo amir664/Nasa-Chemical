34e626e71ffc808fd090bf2234b838d4f6148609
pawankhatri123 <khatripawan7968@gmail.com>
2025-01-27 13:00:49 +0500

    .

--- customer_target_report/report/report.py
+++ customer_target_report/report/report.py
@@ -10,8 +10,8 @@ class CustomReport(models.AbstractModel):
         customer = data['customer']
         start_date = data['start_date']
         end_date = data['end_date']
-        sales_target = data['sales_target',0]
-        current_sales = data['current_sales',0]
+        sales_target = data['sales_target']
+        current_sales = data['current_sales']
 
         others = {}
 
