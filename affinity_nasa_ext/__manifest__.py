e920db2cee51d57fc14fadd29813ab680f46f53d
Abs-ihorizon <107703214+SSubhan-val@users.noreply.github.com>
2025-01-31 21:27:32 +0500

    Update __manifest__.py

--- affinity_nasa_ext/__manifest__.py
+++ affinity_nasa_ext/__manifest__.py
@@ -2,7 +2,7 @@
     'name' : 'Affinity Nasa Extension',
     'version' : '1.0.0',
     'summary' : 'For Customization Purpose',
-    'depends' : ['base', 'product', 'purchase_request', 'quality_control'],
+    'depends' : ['base', 'product', 'quality_control'],
     'data' : ['views/affinity_ext.xml',],
     'installable' : True,
     'application' : True,
