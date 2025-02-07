6716c452f65e979d6767e261455a5f15035dd652
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-07 23:45:25 +0500

    Update __manifest__.py

--- affinity_ext/__manifest__.py
+++ affinity_ext/__manifest__.py
@@ -2,7 +2,7 @@
     'name' : 'Affinity Fields Extension',
     'version' : '1.0.0',
     'summary' : 'For Customization Purpose',
-    'depends' : ['base', 'product','stock','purchase'],
+    'depends' : ['base', 'product','stock','purchase','purchase.request'],
     'data' : ['views/affinity_ext.xml',],
     'installable' : True,
     'application' : True,
