83008110e5a6ad3c9a3dd8eff23e661229bcd148
Abs-ihorizon <107703214+SSubhan-val@users.noreply.github.com>
2025-02-18 17:49:04 +0500

    .

--- customer_sequence/models/res_company.py
+++ customer_sequence/models/res_company.py
@@ -26,6 +26,5 @@ class CompanySequence(models.Model):
     """Inherited Company for adding fields."""
     _inherit = 'res.company'
 
-    customer_code = fields.Integer(string='Customer code', required=True,
-                                   help='Add the required customer code.')
+    customer_code = fields.Integer(string='Customer code', required=True,help='Add the required customer code.')
     next_code = fields.Integer(string='Next code', help='Expected next code')
