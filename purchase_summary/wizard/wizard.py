a2bf462c9e930c1ddac4d73841a1f3fff9aed6c0
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-19 16:46:23 +0500

    Update wizard.py

--- purchase_summary/wizard/wizard.py
+++ purchase_summary/wizard/wizard.py
@@ -25,17 +25,15 @@ class PSReportWizard(models.TransientModel):
         if self.item_wise:
             domain.append(('order_line.product_id', 'in', self.item_wise.ids))
 
-        # Fetch purchase order data based on filters
-        purchase_orders = self.env['purchase.order'].search(domain)
-
-        # Collect unique item names
-        item_names = set()
-        for po in purchase_orders:
-            for line in po.order_line:
-                if not self.item_wise or line.product_id in self.item_wise:
-                    item_names.add(line.product_id.name)
+        data = {
+            'date_from': self.date_from,
+            'date_to': self.date_to,
+            'item_wise': self.item_wise,
+            'vendor_id': self.vendor_id
+            
+            }
 
         # Return report action with item names only
         return self.env.ref('summary_report.summary_report_pdf').with_context(landscape=True).report_action(
-            self, data={'items': list(item_names)}
+            self, data=data
         )
