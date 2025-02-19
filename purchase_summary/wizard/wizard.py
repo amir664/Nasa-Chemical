365d3c38d862819e3d36277a03882ad2f7e9cea1
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-19 16:12:46 +0500

    finalize purchase month wise, asfiyan

--- purchase_summary/wizard/wizard.py
+++ purchase_summary/wizard/wizard.py
@@ -7,10 +7,8 @@ class PSReportWizard(models.TransientModel):
     date_from = fields.Date(string="From Date")
     date_to = fields.Date(string="To Date")
     vendor_id = fields.Many2one('res.partner', string="Vendor", domain=[('supplier_rank', '>', 0)])
-    item_wise = fields.Boolean(string="Item Wise")
-    vendor_accounts_wise = fields.Boolean(string="Vendor Accounts Wise")
-    vendor_group_wise = fields.Boolean(string="Vendor Group Wise")
-    item_group_wise = fields.Boolean(string="Group of Item Wise")
+    item_wise = fields.Many2many('product.product', string="Item(s)")
+
     
     def print_report(self):
         """
@@ -24,6 +22,8 @@ class PSReportWizard(models.TransientModel):
             domain.append(('date_order', '<=', self.date_to))
         if self.vendor_id:
             domain.append(('partner_id', '=', self.vendor_id.id))
+        if self.item_wise:
+            domain.append(('order_line.product_id', 'in', self.item_wise.ids))
 
         # Fetch purchase order data based on filters
         purchase_orders = self.env['purchase.order'].search(domain)
@@ -32,6 +32,10 @@ class PSReportWizard(models.TransientModel):
         report_data = []
         for po in purchase_orders:
             for line in po.order_line:
+                # If specific items are selected, filter only those
+                if self.item_wise and line.product_id not in self.item_wise:
+                    continue
+
                 entry = {
                     'vendor': po.partner_id.name,
                     'po': po.name,
@@ -41,18 +45,21 @@ class PSReportWizard(models.TransientModel):
                     'amount': line.price_total
                 }
 
+                # Determine grouping
                 if self.item_wise:
                     entry['grouping'] = "Item Wise"
-                elif self.vendor_accounts_wise:
+                elif hasattr(self, 'vendor_accounts_wise') and self.vendor_accounts_wise:
                     entry['grouping'] = "Vendor Accounts Wise"
-                elif self.vendor_group_wise:
+                elif hasattr(self, 'vendor_group_wise') and self.vendor_group_wise:
                     entry['grouping'] = "Vendor Group Wise"
-                elif self.item_group_wise:
+                elif hasattr(self, 'item_group_wise') and self.item_group_wise:
                     entry['grouping'] = "Group of Item Wise"
                 else:
                     entry['grouping'] = "General"
 
                 report_data.append(entry)
 
-        # Return report action
-        return self.env.ref('summary_report.summary_report_pdf').with_context(landscape=True).report_action(self, data={'report_data': report_data})
+        # Return report action with formatted data
+        return self.env.ref('summary_report.summary_report_pdf').with_context(landscape=True).report_action(
+            self, data={'report_data': report_data}
+        )
