ab78eec82e9cce2b79c0634f521f9c82e4b6ada4
Abs-ihorizon <107703214+SSubhan-val@users.noreply.github.com>
2025-02-19 11:53:08 +0500

    .

--- affinity_customer_target/models/customer_target.py
+++ affinity_customer_target/models/customer_target.py
@@ -6,12 +6,25 @@ class CustomerTarget(models.Model):
     _description="Customer Target App"
 
     name = fields.Char(string="Name", required=True)
-    target_count = fields.Integer(string="Target Count", required=True)
     start_date = fields.Date(string="Start Date",)
     end_date = fields.Date(string="End Date",)
     customer = fields.Many2one('res.partner', string="Customer",)
     line_ids = fields.One2many('customer.target.line','customer_target_id', sting="Line Ids", required=True)
     
+
+class CustomerTargetLine(models.Model):
+    _name="customer.target.line"
+    _description = "Customer Target Line"
+
+    customer_target_id = fields.Many2one('customer.target', required=True)
+    product_id = fields.Many2one('product.product', required=True)
+    target = fields.Float(string="Target", required=True)
+    
+class ResPartner(models.Model):
+    
+    _inherit = 'res.partner'
+    
+    target_count = fields.Integer(string="Target Count", required=True)
     def open_customer_targets(self):
         return {
             'name': 'Customer Target',
@@ -27,12 +40,4 @@ class CustomerTarget(models.Model):
         count = self.env['res.partner'].search_count([('customer', '=', self.id)])
         self.target_count = count
 
-
-class CustomerTargetLine(models.Model):
-    _name="customer.target.line"
-    _description = "Customer Target Line"
-
-    customer_target_id = fields.Many2one('customer.target', required=True)
-    product_id = fields.Many2one('product.product', required=True)
-    target = fields.Float(string="Target", required=True)
     
