a8e8bf09f8f27d9b01109e4105f9539029d1a40d
Abs-ihorizon <107703214+SSubhan-val@users.noreply.github.com>
2025-02-19 11:32:27 +0500

    .

--- affinity_customer_target/models/customer_target.py
+++ affinity_customer_target/models/customer_target.py
@@ -17,17 +17,6 @@ class CustomerTargetLine(models.Model):
     _description = "Customer Target Line"
 
     customer_target_id = fields.Many2one('customer.target', required=True)
-    sales_target = fields.Float(string="Sales Target", required=True)
-    # current_sales = fields.Float( string = "Current Sales", readonly=True, required=True, compute= "get_sales_target_sum")
-
-    # @api.depends('customer',"customer_target_id.sale_person_id") 
-    # def get_sales_target_sum(self ):
-    #     for rec in self:
-    #         rec.current_sales = 0
-            
-    #         records = rec.env['sale.order'].search([('user_id', '=', rec.customer_target_id.sale_person_id.id),('partner_id', '=', rec.customer.id),('state', '!=', 'cancel')])
-    #         if records:
-                    
-    #             total_sales_target = sum(records.mapped('amount_total'))
-            
-    #             rec.current_sales = total_sales_target
+    product_id = fields.Many2one('product.product', required=True)
+    target = fields.Float(string="Target", required=True)
+    
