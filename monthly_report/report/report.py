9f5bbd306396c45e0d587c903dd0d75dc749a1e9
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-19 11:37:23 +0500

    Update report.py

--- monthly_report/report/report.py
+++ monthly_report/report/report.py
@@ -189,7 +189,7 @@ class CustomReport(models.AbstractModel):
         
         cr.execute(query)
         data = cr.dictfetchall()
-        raise UserError(data);
+        raise UserError(data)
         
         # Calculate total quantities and amounts dynamically
         totals = {}
