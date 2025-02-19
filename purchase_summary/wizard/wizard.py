93b998fd6765c69c41b3d9ba513340ffc40fba35
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-19 16:53:20 +0500

    Update wizard.py

--- purchase_summary/wizard/wizard.py
+++ purchase_summary/wizard/wizard.py
@@ -14,24 +14,18 @@ class PSReportWizard(models.TransientModel):
         """
         Generates the purchase order summary report based on the selected filters.
         """
+        purchase_orders = self.env['purchase.order']
 
-        domain = []
-        if self.date_from:
-            domain.append(('date_order', '>=', self.date_from))
-        if self.date_to:
-            domain.append(('date_order', '<=', self.date_to))
-        if self.vendor_id:
-            domain.append(('partner_id', '=', self.vendor_id.id))
-        if self.item_wise:
-            domain.append(('order_line.product_id', 'in', self.item_wise.ids))
+        item_ids = []
+        for po in purchase_orders:
+            for line in po.order_line:
+                if not self.item_wise or line.product_id in self.item_wise:
+                    item_ids.append(line.product_id.id)
 
+    
         data = {
-            'date_from': self.date_from,
-            'date_to': self.date_to,
-            'item_wise': self.item_wise,
-            'vendor_id': self.vendor_id
-            
-            }
+        'items': item_ids,  # Sending only item IDs
+        }
 
         # Return report action with item names only
         return self.env.ref('summary_report.summary_report_pdf').with_context(landscape=True).report_action(
