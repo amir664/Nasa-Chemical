4c002d6536e2341706930a29209f742d8b3f81ba
osama <ok26568@gmail.com>
2025-02-20 17:09:06 +0500

    ..

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -138,23 +138,27 @@ class SaleOrderInherit(models.Model):
 class SaleOrderLineInherit(models.Model):
     _inherit = "sale.order.line"
 
-    discount = fields.Float(string="Discount")
-    discount_in_amount = fields.Float(string="Discount in Amount", compute="_compute_discount_amount", store=True)
-   
+    # discount = fields.Float(string="Discount")
+    # discount_in_amount = fields.Float(string="Discount in Amount", compute="_compute_discount_amount", store=True)
+    discount = fields.Float(string="Discount (%)", compute="_compute_discount_percentage", store=True)
+    discount_in_amount = fields.Float(string="Discount in Amount")
     # @api.onchange('discount')
     # def _onchange_discount_amount(self):
     #     for line in self:
     #         if line.discount:
     #             line.price_total = line.price_subtotal - line.discount
 
-    @api.depends('price_unit', 'product_uom_qty', 'discount')
-    def _compute_discount_amount(self):
+    @api.depends('price_unit', 'product_uom_qty', 'discount_in_amount')
+    def _compute_discount_percentage(self):
         for line in self:
-            line.discount_in_amount = (line.price_unit * line.product_uom_qty) * (line.discount / 100.0)
+            total_price = line.price_unit * line.product_uom_qty
+            if total_price > 0:
+                line.discount = (line.discount_in_amount / total_price) * 100
+            else:
+                line.discount = 0.0
 
     @api.depends('price_unit', 'product_uom_qty', 'discount_in_amount', 'tax_id')
     def _compute_amount(self):
         for line in self:
-            super(SaleOrderLineInherit, line)._compute_amount() 
-            if line.discount_in_amount:
-                line.price_subtotal = max(0, line.price_subtotal - line.discount_in_amount)  
+            super(SaleOrderLineInherit, line)._compute_amount()  
+            line.price_subtotal = max(0, line.price_subtotal - line.discount_in_amount)
