3ebe61f83b928dde4c45cfc6b813f27acbeec7b4
osama <ok26568@gmail.com>
2025-02-20 16:51:10 +0500

    ..

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -139,9 +139,21 @@ class SaleOrderLineInherit(models.Model):
     _inherit = "sale.order.line"
 
     discount = fields.Float(string="Discount")
+    discount_in_amount = fields.Float(string="Discount in Amount", compute="_compute_discount_amount", store=True)
+   
+    # @api.onchange('discount')
+    # def _onchange_discount_amount(self):
+    #     for line in self:
+    #         if line.discount:
+    #             line.price_total = line.price_subtotal - line.discount
 
-    @api.onchange('discount')
-    def _onchange_discount_amount(self):
+    @api.depends('price_unit', 'product_uom_qty', 'discount')
+    def _compute_discount_amount(self):
         for line in self:
-            if line.discount:
-                line.price_total = line.price_subtotal - line.discount
+            line.discount_in_amount = (line.price_unit * line.product_uom_qty) * (line.discount / 100.0)
+
+    @api.depends('price_unit', 'product_uom_qty', 'discount_in_amount', 'tax_id')
+    def _compute_amount(self):
+        for line in self:
+            super(SaleOrderLineInherit, line)._compute_amount()  
+            line.price_subtotal -= line.discount_in_amount  
