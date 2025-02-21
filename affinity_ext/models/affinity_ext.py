c69312ce11b31f10ea548441ef09af4f992d32e8
osama <ok26568@gmail.com>
2025-02-21 12:38:31 +0500

    Update affinity_ext.py

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -142,12 +142,20 @@ class SaleOrderLineInherit(models.Model):
     _inherit = "sale.order.line"
 
    
-    discount = fields.Float(string="Discount (%)", compute="_compute_amount", store=True)
+    discount = fields.Float(string="Discount (%)", compute="_compute_discount_percentage", store=True)
     discount_in_amount = fields.Float(string="Discount in Amount")
 
 
-    @api.depends('price_unit', 'product_uom_qty', 'discount_in_amount', 'tax_id')
-    def _compute_amount(self):
+    @api.depends('discount_in_amount', 'price_unit', 'product_uom_qty', 'tax_id')
+    def _compute_discount_percentage(self):
         for line in self:
-            super(SaleOrderLineInherit, line)._compute_amount()  
-            line['discount'] = (line.discount_in_amount/line.price_subtotal)*100
+            subtotal = (line.price_unit * line.product_uom_qty)  
+            if subtotal > 0:
+                line.discount = (line.discount_in_amount / subtotal) * 100
+            else:
+                line.discount = 0.0
+    # @api.depends('price_unit', 'product_uom_qty', 'discount_in_amount', 'tax_id')
+    # def _compute_amount(self):
+    #     for line in self:
+    #         super(SaleOrderLineInherit, line)._compute_amount()  
+    #         line['discount'] = (line.discount_in_amount/line.price_subtotal)*100
