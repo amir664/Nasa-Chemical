119c81caa5c3891ca2eff94c2612cc4dd6c0a396
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-22 17:13:24 +0500

    advance vendor report creating

---
+++ advance_vendor_rport/wizard/wizard.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api
+
+class PSReportWizard(models.TransientModel):
+    _name = 'advanceVendor.report'
+    _description = 'Advance to Vendor Report'
+    
+    date_from = fields.Date(string="From Date")
+    date_to = fields.Date(string="To Date")
+    vendor_id = fields.Many2one('res.partner', string="Vendor", domain=[('supplier_rank', '>', 0)])
+
+
+    def generate_excel_report(self):
+        return {
+            'type': 'ir.actions.act_url',
+            'url': '/purchase_summary/excel_report?date_from={}&date_to={}&vendor_id={}&item_wise={}'.format(
+                self.date_from or '',
+                self.date_to or '',
+                self.vendor_id.id or '',
+                self.item_wise.id or ''  
+            ),
+            'target': 'new'
+        }
