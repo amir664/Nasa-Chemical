9478735afe766a1cfe7a77a06f514fc6905e660c
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-22 17:55:40 +0500

    solve pr

--- purchase_manualreport/wizard/wizard.py
+++ purchase_manualreport/wizard/wizard.py
@@ -9,9 +9,9 @@ class PurchaseReportWizard(models.TransientModel):
     date_to = fields.Date(string='To Date', required=True)
     product_ids = fields.Many2many('product.template', string='Product', required=True)
     vendor_ids = fields.Many2many('res.partner', string = "Vendor", required=True)
-    po_no = fields.Many2one("purchase.order", required=True)
-    grn = fields.Many2one("stock.picking", required=True)
-    invoice_no = fields.Many2one("account.move", required=True)
+    po_no = fields.Many2many("purchase.order", required=True)
+    grn = fields.Many2many("stock.picking", required=True)
+    invoice_no = fields.Many2many("account.move", required=True)
     
 
     def print_report(self):
