b162f8d45a62e4a76e45e4a85008144301efbc91
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-22 18:38:19 +0500

    add due date field

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -57,7 +57,7 @@ class StockPickingInherited(models.Model):
 class PurchaseOrderInherited(models.Model):
     _inherit = "purchase.order"
 
-
+    custom_date = fields.Date(string="Custom Date")
     
     notes = fields.Html(
         string="Terms and Condition" 
