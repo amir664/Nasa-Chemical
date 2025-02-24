18da2cf66735a81fc94600116a35f2cb511937f1
Abs-ihorizon <107703214+SSubhan-val@users.noreply.github.com>
2025-02-24 13:12:09 +0500

    Update affinity_ext.py

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -11,7 +11,7 @@ class ResPartnerInherited(models.Model):
     artical = fields.Char("Article of Association and Memorandom", required = True)
     strn_no = fields.Char("STRN", required = True)
     cnic_no = fields.Char("CNIC Number", required = True)
-    vendor_status = fields.Selection([('active', 'Active'), ('non_active', 'Non Active')], string = "Status")
+    vendor_status = fields.Selection([('active', 'Active'), ('non_active', 'Non Active')], string = "Active Status")
     major_client = fields.Selection([('a','A Category'), ('b', 'B Category'), ('c', 'C Category')]) 
     region = fields.Char(string="Region")
     status = fields.Selection([('DISTRIBUTOR','DISTRIBUTOR'), ('DEALER', 'DEALER'), ('W.SELLER', 'W.SELLER'), ('WHOLESELLER', 'WHOLESELLER')]) 
