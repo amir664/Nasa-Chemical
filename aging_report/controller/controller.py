bb391b95e8aad0d4ab51c55c74af93c0e5a7b9dc
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-24 13:53:12 +0500

    Update controller.py

--- aging_report/controller/controller.py
+++ aging_report/controller/controller.py
@@ -58,7 +58,7 @@ class AgingReportController(http.Controller):
 
 
             # Fetch payments (Fixing KeyError issue)
-            payments = request.env['account.payment'].search([('ref', 'in', (po.invoice_ids.name))])
+            payments = request.env['account.payment'].search([('ref', 'in', [po.invoice_ids.name])])
             payment_ref = 0#', '.join(payments.mapped('ref'))  # Fix: Use 'name' instead of 'communication'
             # payment_ref = ', '.join([ref for ref in payments.mapped('ref') if ref])
             # raise UserError(invoice)
