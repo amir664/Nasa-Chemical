3a410fea8a4bacbd12b360e63b798667e84e1735
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-24 14:11:39 +0500

    Update controller.py

--- aging_report/controller/controller.py
+++ aging_report/controller/controller.py
@@ -61,6 +61,7 @@ class AgingReportController(http.Controller):
             payments = request.env['account.payment'].search([
                 ('move_id.line_ids.move_id', 'in', po.invoice_ids.ids)
             ])
+            raise UserError(payments)
             # payment_ref = ', '.join(payments.mapped('ref'))  # Fix: Use 'name' instead of 'communication'
             payment_ref = ', '.join(filter(None, payments.mapped('ref')))
 
