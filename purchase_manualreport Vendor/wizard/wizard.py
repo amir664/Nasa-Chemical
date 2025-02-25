2d7a98845cecd285bfdaae137ec9bcfa5d5b1c71
Asfiyan Shivani <asfiyanshivani0321@gmail.com>
2025-02-25 17:36:34 +0500

    purchase vendor

--- purchase_manualreport Vendor/wizard/wizard.py
+++ purchase_manualreport Vendor/wizard/wizard.py
@@ -2,7 +2,7 @@ from odoo import _, api, fields, models
 
 
 class PurchaseReportWizard(models.TransientModel):
-    _name = 'purchase.manualreport'
+    _name = 'purchase_vendor.manualreport'
     _description = 'Purchase Report'
     
     date_from = fields.Date(string='From Date')
