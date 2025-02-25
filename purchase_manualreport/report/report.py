a2d9acdfef7688372bafa7f1a6a5d2b54075e1d9
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-25 16:15:55 +0500

    Update report.py

--- purchase_manualreport/report/report.py
+++ purchase_manualreport/report/report.py
@@ -70,7 +70,7 @@ class CustomReport(models.AbstractModel):
                 
         
         if grn != "stock.picking()":
-            raise UserError(grn)  
+            raise UserError(grn.name)  
             po_numbers = grn.replace("stock.picking(", "").replace(")", "").strip()
             raise UserError(po_numbers)
             if po_numbers:  
