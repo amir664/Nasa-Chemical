08247b3f8f2ad65cee1a5cdc1f24534ca6e609ca
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-25 16:39:47 +0500

    Update report.py

--- purchase_manualreport/report/report.py
+++ purchase_manualreport/report/report.py
@@ -70,6 +70,7 @@ class CustomReport(models.AbstractModel):
                 
         
         if grn:
+            raise UserError(grn.id)
     # Extract names from selected grn records
             grn_names = tuple(rec.name for rec in grn if hasattr(rec, 'name') and rec.name)
 
