f8787bc38f28d91385e553c61edd7127dc5d6c0c
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-25 11:02:02 +0500

    Update report.py

--- purchase_manualreport/report/report.py
+++ purchase_manualreport/report/report.py
@@ -46,12 +46,12 @@ class CustomReport(models.AbstractModel):
             params.extend([tuple(date_from), tuple(date_to)])
 
         if product_ids:
-            where_clauses.append("pt.id IN %s")
+            where_clauses.append(f"pt.id IN {product_ids}")
             # raise UserError([product_ids,vendor_ids])
-            params.append(product_ids)  # Tuple for SQL IN clause
+            # params.append(product_ids)  # Tuple for SQL IN clause
 
         if vendor_ids:
-            where_clauses.append("rs.id IN %s")
+            where_clauses.append(f"rs.id IN {vendor_ids}")
             params.append(vendor_ids)
         # raise UserError(po_no)
         # raise UserError([po_no,grn,invoice_no,vendor_ids,product_ids,date_from])
