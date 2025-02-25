76e71e0559e573564191f540fbff86a59e9a4aa4
Bilal Memon <73229916+BilalMemon07@users.noreply.github.com>
2025-02-25 13:34:55 +0500

    vendor product filter working

--- purchase_manualreport/wizard/wizard.py
+++ purchase_manualreport/wizard/wizard.py
@@ -7,8 +7,8 @@ class PurchaseReportWizard(models.TransientModel):
     
     date_from = fields.Date(string='From Date')
     date_to = fields.Date(string='To Date')
-    product_ids = fields.Many2many('product.template', string='Product')
-    vendor_ids = fields.Many2many('res.partner', string = "Vendor")
+    product_ids = fields.Many2one('product.template', string='Product')
+    vendor_ids = fields.Many2one('res.partner', string = "Vendor")
     po_no = fields.Many2many("purchase.order")
     grn = fields.Many2many("stock.picking")
     invoice_no = fields.Many2many("account.move")
