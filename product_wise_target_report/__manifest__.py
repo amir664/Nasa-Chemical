81edd936d7fda4235da19c7addb73767def8b59f
osama <ok26568@gmail.com>
2025-02-25 14:47:53 +0500

    ..

---
+++ product_wise_target_report/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name':'Product Wise Target Report',
+    'version':'17.0.1.0.0',
+    'depends': ['base','sale'],
+    'data':[
+        'wizard/wizard.xml',
+        'security/ir.model.access.csv',
+        # 'report/report.xml',
+        # 'report/report_template.xml'
+    ],
+    'installable':True,
+    'auto-install':False,
+    'application':False
+}
