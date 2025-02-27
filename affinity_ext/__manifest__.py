aa453f3f2476ff29ecf570e194b8d12d5a99ec1c
Abs-ihorizon <107703214+SSubhan-val@users.noreply.github.com>
2025-02-27 11:47:11 +0500

    Update __manifest__.py

--- affinity_ext/__manifest__.py
+++ affinity_ext/__manifest__.py
@@ -3,7 +3,7 @@
     'version' : '1.0.0',
     'summary' : 'For Customization Purpose',
     'depends' : ['base', 'product','stock','purchase','purchase_request','sale'],
-    'data' : ['views/affinity_ext.xml',],
+    'data' : ['views/affinity_ext.xml','security/ir.model.access.csv'],
     'installable' : True,
     'application' : True,
     'License' : 'LGPL-3'
