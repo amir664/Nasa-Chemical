e27d384020a443fa60ebad453085cb142b811e38
Abs-ihorizon <107703214+SSubhan-val@users.noreply.github.com>
2025-02-27 12:59:44 +0500

    Update wizard.py

--- sales_target_report/wizard/wizard.py
+++ sales_target_report/wizard/wizard.py
@@ -7,7 +7,7 @@ class SalesTargetWizard(models.TransientModel):
 
     date_from = fields.Date('Date From', required=True)
     date_to = fields.Date('Date To', required=True)
-    region_ids = fields.Many2many('res.customer.region',string="Region")
+    region_ids = fields.Many2many('res.customer.region','company_id',string="Region")
     sub_region_ids = fields.Many2many('res.customer.region',string="Sub-Region")
     customer_ids = fields.Many2many('res.partner', string = "Customers")
     status = fields.Selection([('DISTRIBUTOR','DISTRIBUTOR'), ('DEALER', 'DEALER'), ('W.SELLER', 'W.SELLER'), ('WHOLESELLER', 'WHOLESELLER')]) 
