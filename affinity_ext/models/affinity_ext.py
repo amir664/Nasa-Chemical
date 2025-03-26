ec6d02e9b4b0ad0493607e77e6f434d2761edcc9
Asfiyan Shivani <odoo.sh@affinitysuite.net>
2025-03-27 00:51:32 +0500

    Update affinity_ext.py

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -189,14 +189,14 @@ class SaleOrder(models.Model):
     @api.depends_context('uid')
     def _compute_can_approve(self):
         """Compute if the current user can approve."""
-        approved_users = ['Amanullah Khan', 'Fahad Humayoun','Administrator']
+        approved_users = ['Amanullah Khan', 'Fahad Humayoun','Administrator','Administrator']
         current_user = self.env.user.partner_id.name
         for order in self:
             order.can_approve = current_user in approved_users
 
     def action_approve_order(self):
         """Approve the order by Amanullah and Fahad only."""
-        approved_users = [['Amanullah Khan', 'Fahad Humayoun','Administrator']]
+        approved_users = [['Amanullah Khan', 'Fahad Humayoun','Administrator','Administrator']]
         current_user = self.env.user.partner_id
 
         if current_user.name not in approved_users:
