c9f524336d97f6ec8cb6bd9d1472af9840cbe79d
Asfiyan Shivani <asfiyanshivani0321@gmail.com>
2025-04-08 13:45:45 +0500

    Update __manifest__.py

--- affinity_ext/__manifest__.py
+++ affinity_ext/__manifest__.py
@@ -2,7 +2,7 @@
     'name' : 'Affinity Fields Extension',
     'version' : '1.0.0',
     'summary' : 'For Customization Purpose',
-    'depends' : ['base', 'product','stock','purchase','purchase_request','sale'],
+    'depends' : ['base', 'product','stock','purchase','purchase_request','sale','quality_control'],
     'data' : ['security/ir.model.access.csv','views/affinity_ext.xml'],
     'installable' : True,
     'application' : True,
