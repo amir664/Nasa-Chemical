bfe9e1121c978dbee31f55f0b888558494030fbc
Asfiyan Shivani <asfiyanshivani0321@gmail.com>
2025-04-08 13:32:44 +0500

    Update affinity_ext.py

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -191,4 +191,5 @@ class CustomerRegion(models.Model):
 #         related='point_id.tolerance_min', 
 #         string='Minimum Tolerance',
 #         readonly=True
-    # )    
+    # )   
+    #  
