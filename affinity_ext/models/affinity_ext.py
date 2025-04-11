8fc6d1c2b1ee3a1363064b7a1e1f32b773ca504f
sibtehunain <hunainsibte64@gmail.com>
2025-04-11 15:59:44 +0500

    Update affinity_ext.py

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -176,6 +176,54 @@ class QualityCheckInherited(models.Model):
 
     methods = fields.Char(string="Method")
 
+    @api.model
+    def create(self, vals):
+        res = super(QualityCheckInherited, self).create(vals)
+        for rec in self:
+            rules = self.env['my.model.main'].search([('model_name', '=', 'quality.check')])
+            for rule in rules:
+                for line in rule.line_ids:
+            # raise UserError(str(rules.name))
+                # try:
+                    local_dict = {'record': rec,
+                                  'self':rule
+                                  }
+                    # raise UserError(str(local_dict))
+                    if safe_eval(line.condition, local_dict):
+                        message = line.message_template.format(record=rec)
+                        rec.message_post(
+                            body=message,
+                            partner_ids=rule.user_ids.mapped('partner_id').ids,
+                            subtype_xmlid="mail.mt_comment"
+                        )
+            # rules.check_notification(rec)
+
+        return res
+
+    # def write(self, vals):
+    #     res = super(QualityCheckInherited, self).write(vals)
+    #     for rec in self:
+    #         rules = self.env['my.model.main'].search([('model_name', '=', 'quality.check')])
+    #         for rule in rules:
+    #             for line in rule.line_ids:
+    #         # raise UserError(str(rules.name))
+    #             # try:
+    #                 local_dict = {'record': rec,
+    #                               'self':rule
+    #                               }
+    #                 # raise UserError(str(local_dict))
+    #                 if safe_eval(line.condition, local_dict):
+    #                     message = line.message_template.format(record=rec)
+    #                     rec.message_post(
+    #                         body=message,
+    #                         partner_ids=rule.user_ids.mapped('partner_id').ids,
+    #                         subtype_xmlid="mail.mt_comment"
+    #                     )
+    #         # rules.check_notification(rec)
+    #     return res
+
+
+
 
 class SaleOrderInherit(models.Model):
     _inherit = "sale.order"
