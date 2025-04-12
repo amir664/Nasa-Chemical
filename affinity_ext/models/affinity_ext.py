d2a834cf7b6af574203f91c7229f42ead86f23db
sibtehunain <hunainsibte64@gmail.com>
2025-04-12 12:05:09 +0500

    add salesperson

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -134,6 +134,7 @@ class SaleOrderInherit(models.Model):
     _inherit = "sale.order"
 
     delivery_address = fields.Char(string="Delivery Address", compute="_compute_delivery_address", readonly=False)
+    salesperson = fields.Char(string="Salesperson")
 
     def _compute_delivery_address(self):
         for record in self:
