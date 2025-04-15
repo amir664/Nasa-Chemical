e3880cd49c91e8ce02aade2aafc24cc3486e84f0
sibtehunain <hunainsibte64@gmail.com>
2025-04-15 12:46:00 +0500

    Update affinity_ext.py

--- affinity_ext/models/affinity_ext.py
+++ affinity_ext/models/affinity_ext.py
@@ -262,9 +262,9 @@ class SaleOrderLineInherit(models.Model):
 
 
 class ResUser(models.Model):
-    _name = "res.user"
+    _name = "res.users"
     location = fields.Many2one('stock.location',string="Location")
-    
+
 class CustomerRegion(models.Model):
     _name = "res.customer.region"
     
