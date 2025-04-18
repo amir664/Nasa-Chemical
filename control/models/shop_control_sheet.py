742b1c8d7f195ccc2028c85ce8a5fa2582f324e7
Asfiyan Shivani <asfiyanshivani0321@gmail.com>
2025-04-18 15:11:13 +0500

    Update shop_control_sheet.py

--- control/models/shop_control_sheet.py
+++ control/models/shop_control_sheet.py
@@ -6,8 +6,8 @@ class ShopControlSheet(models.Model):
     _name = 'control.shop_control_sheet'
     _description = 'Shop Control Sheet'
 
-    name = fields.Date(string='Shop Control Dated', required=True)
-    control_officer = fields.Char(string='Control Officer', required=True)
+    name = fields.Date(string='Shop Control Dated')
+    control_officer = fields.Char(string='Control Officer')
     toilet_cleaning_ids = fields.One2many('control.toilet_cleaning', 'sheet_id', string='Toilet Cleaning')
 
 
@@ -16,7 +16,7 @@ class ToiletCleaning(models.Model):
     _name = 'control.toilet_cleaning'
     _description = 'Toilet Cleaning Checklist'
 
-    sheet_id = fields.Many2one('control.shop_control_sheet', string='Control Sheet', required=True)
+    sheet_id = fields.Many2one('control.shop_control_sheet', string='Control Sheet')
     item = fields.Char(default='Tissue Paper In Toilet', readonly=True)
     yes_no = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Yes/No')  # <- no required=True
     condition = fields.Selection([('good', 'Good'), ('bad', 'Bad')], string='Condition')  # <- no required=True
