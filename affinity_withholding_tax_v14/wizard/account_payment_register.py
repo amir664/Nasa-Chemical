d2c0513f083b99ef3d9c1c78e41ff39e079a9d58
sibtehunain <hunainsibte64@gmail.com>
2025-04-26 10:54:49 +0500

    Update account_payment_register.py

--- affinity_withholding_tax_v14/wizard/account_payment_register.py
+++ affinity_withholding_tax_v14/wizard/account_payment_register.py
@@ -9,6 +9,24 @@ class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
 
     wht_lines = fields.One2many('withholding.lines','wizard_id',string="Withholding Lines")
+    wht_account = fields.Many2one('account.account',string="Witholding Account")
+
+    # @api.model
+    # def default_get(self, fields_list):
+    #     defaults = super().default_get(fields_list)
+    #     wizard_id = self.env.context.get('default_wizard_id')
+
+    #     if wizard_id:
+    #         wizard = self.env['account.payment.register'].browse(wizard_id)
+    #         if wizard.communication:
+    #             account_move = self.env['account.move'].search([('name', '=', wizard.communication)], limit=1)
+    #             if account_move.move_type == 'out_invoice':
+    #                 defaults['wht_account'] = 8     # Customer invoice
+    #             elif account_move.move_type == 'in_invoice':
+    #                 defaults['wht_account'] = 1157  # Vendor bill
+
+    #     return defaults
+
 
     def _create_payments(self):
         res = super(AccountPaymentRegister, self)._create_payments()
@@ -78,22 +96,7 @@ class Withholdinglines(models.TransientModel):
     wht_code = fields.Many2one('account.tax',string="Tax Code")
     wht_account = fields.Many2one('account.account',string="Witholding Account")
 
-    @api.model
-    def default_get(self, fields_list):
-        defaults = super().default_get(fields_list)
-        wizard_id = self.env.context.get('default_wizard_id')
-
-        if wizard_id:
-            wizard = self.env['account.payment.register'].browse(wizard_id)
-            if wizard.communication:
-                account_move = self.env['account.move'].search([('name', '=', wizard.communication)], limit=1)
-                if account_move.move_type == 'out_invoice':
-                    defaults['wht_account'] = 8     # Customer invoice
-                elif account_move.move_type == 'in_invoice':
-                    defaults['wht_account'] = 1157  # Vendor bill
-
-        return defaults
-
+   
 
 
     @api.onchange('wht_code')
